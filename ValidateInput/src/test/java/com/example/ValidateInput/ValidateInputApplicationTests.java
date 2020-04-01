436140e56e27d37fd11256f998ccf86ec7902abf
madhurimukund97 <madhurimukund97@msitprogram.net>
2020-04-01 17:58:58 +0530

    validateinp

---
+++ ValidateInput/src/test/java/com/example/ValidateInput/ValidateInputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ValidateInput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidateInputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
