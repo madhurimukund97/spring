8e701f8a6048a81d74e02c98a7e9f76d57d79443
madhurimukund97 <madhurimukund97@msitprogram.net>
2020-04-16 12:57:16 +0530

    firstapp

---
+++ JavaFundamentals/FirstApplication/src/FirstApplication/Main.java
@@ -0,0 +1,17 @@
+package FirstApplication;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("even numbers from 1 to 10");
+		for (int i=0;i<=10;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+				
+			}
+		}
+
+	}
+
+}
