bd54b29c206c4512bc871f37449d31ff83916ae1
madhurimukund97 <madhurimukund97@msitprogram.net>
2020-05-07 11:47:57 +0530

    buildingrestfulapp

---
+++ BuildRestful/src/main/java/com/example/BuildRestful/BuildRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.BuildRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildRestfulApplication.class, args);
+	}
+
+}
