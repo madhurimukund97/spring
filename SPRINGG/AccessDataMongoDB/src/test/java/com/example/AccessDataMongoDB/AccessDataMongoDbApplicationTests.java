700cfb018a6a45db4055373adfd3d42115ec3095
madhurimukund97 <madhurimukund97@msitprogram.net>
2020-05-15 13:58:38 +0530

    wfh

---
+++ SPRINGG/AccessDataMongoDB/src/test/java/com/example/AccessDataMongoDB/AccessDataMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccessDataMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessDataMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ SPRINGG/gatewayex/src/main/java/com/example/gatewayex/GatewayexApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewayex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayexApplication.class, args);
+	}
+
+}
